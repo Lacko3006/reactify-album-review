191c1af07534501adf7705b4c2ad04c3ef61dbc9
Kenny Hung (he) <khung@instructors.2u.com>
2023-03-13 21:13:38 +0000

    State variables for login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 
 
 export default function Login() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const usernameChange = e => {
+    setUsername(e.target.value)
+  }
+
+  const emailChange = e => {
+    setEmail(e.target.value)
+  }
+
+  const passwordChange = e => {
+    setPassword(e.target.value)
+  }
+
   return (
-    <section class="wrapper">
+    <section className="wrapper">
       <div className="form signup">
         <header>Signup</header>
         <form action="#">
-          <input id="name-signup" type="text" placeholder="Full name" required />
-          <input id="email-signup" type="text" placeholder="Email address" required />
-          <input id="password-signup" type="password" placeholder="Password" required />
+          <input id="name-signup" type="text" placeholder="username" value={username} onChange={usernameChange} />
+          <input id="email-signup" type="email" placeholder="email" value={email} onChange={emailChange} />
+          <input id="password-signup" type="password" placeholder="password" value={password} onChange={passwordChange} />
           <div className="checkbox">
             <input type="checkbox" id="signupCheck" />
-            <label for="signupCheck">I accept all terms & conditions</label>
+            <label htmlFor="signupCheck">I accept all terms & conditions</label>
           </div>
           <input className="signup-form" type="submit" value="Signup" />
         </form>
