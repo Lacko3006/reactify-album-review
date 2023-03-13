db0134f58da5ca0d164ecc38e04ade6fd34fff67
shuib <shuaib_97@hotmail.co.uk>
2023-03-13 20:18:10 +0000

    login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../index.css";
+
+export default function Login() {
+  return ( <section class="wrapper">
+      <div className="form signup">
+        <header>Signup</header>
+        <form action="#">
+          <input id="name-signup" type="text" placeholder="Full name" required />
+          <input id="email-signup" type="text" placeholder="Email address" required />
+          <input id="password-signup" type="password" placeholder="Password" required />
+          <div className="checkbox">
+            <input type="checkbox" id="signupCheck" />
+            <label for="signupCheck">I accept all terms & conditions</label>
+          </div>
+          <input className="signup-form" type="submit" value="Signup" />
+        </form>
+      </div>
+
+      <div className="form login">
+        <header>Login</header>
+        <form action="#">
+          <input id="email-login" type="text" placeholder="Email address" required />
+          <input id="password-login" type="password" placeholder="Password" required />
+          <a href="#">Forgot password?</a>
+          <input className="login-form" type="submit" value="Login" />
+        </form>
+      </div>
+    </section>
+
+  )
+}
