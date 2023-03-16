24253e73c5f74114fc5de3619036baa04f16afaa
Ben <bgrowcott@hotmail.co.uk>
2023-03-16 18:41:46 +0000

    small style improvements

--- client/src/components/Albums.js
+++ client/src/components/Albums.js
@@ -12,17 +12,21 @@ export default function Albums() {
                 <h1>loading...</h1>
             ) : (
                 <>
-                    <h1>Albums</h1>
+                    <h1 className="text-light">Albums</h1>
 
                     <div className="album-details row g-2">
                         {albumData.getAlbums.map(it => {
                           return (
                               <div className="col-12 col-sm-6 col-md-4">
-                                <div className="bg-light text-dark">
-                                  <div>{it.name}</div>
-                                  <div>{it.genre}</div>
-                                  <div>{it.year}</div>
+                                <div className="bg-light text-dark p-2 rounded">
+                                  <div>Name: {it.name}</div>
+                                  <div>Genre: {it.genre}</div>
+                                  <div>Year: {it.year}</div>
                                   {/* <div>{it.songs}</div> */}
+                                  <details>
+                                    <summary>Songs</summary>
+                                    {it.songs.map(song => <p className="text-dark">{song}</p>)}
+                                  </details>
                                 </div>
                               </div>                            
                           )
