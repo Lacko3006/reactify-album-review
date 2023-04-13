2cfc5c13b5f6a1e07b14afc003ed17eb66cf810a
Nuseyba <n.kenadiid@gmail.com>
2023-04-13 17:36:30 +0100

    edited Homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,25 +2,25 @@ import React from "react";
 import "../index.css";
 
 let styles = {
-    marginLeft: "196px",
-}
+  marginLeft: "196px",
+};
 
 export default function Homepage() {
   return (
-    <div  style={styles}>
+    <div style={styles}>
       <section id="hero">
         <div className="container">
           <div className="info">
             <h1>Welcome to RhythmReview</h1>
             <h2>Using Rythem Raters</h2>
             <p>
-              Rythem Raters mission is to unlock the potential of human
-              creativity by giving a million creative artists the opportunity to
-              live off their art and billions of fans the opportunity to enjoy
-              and be comment on the art these artists come up with. We believe
-              achieving this mission on our platform is made possible by
-              welcoming different forms of fanatical expression, ideas,
-              perspectives and voices.
+              Rythym Raters mission is to unlock the potential of human
+              creativity by giving millions of creative artists the opportunity to
+              live off their art while also allowing their fans the opportunity to enjoy
+              and as well as comment. Our users can alphabetically view a plethora of
+              artist and albums and peruse to their heart's content. They can
+              also opt to use our search button allowing them to view a
+              particular search with ease.
             </p>
             <a href="/album">Review today</a>
           </div>
